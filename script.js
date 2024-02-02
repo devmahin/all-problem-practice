b586a6681dce4a2538cf4a9b8cc6de77ae53cc7b
devmahin <mahinhowlader123@gmail.com>
2024-02-02 10:30:24 +0600

    js condition task

--- script.js
+++ script.js
@@ -7,13 +7,13 @@ Free Drinks
 
 
 
-let bill = 500;
-if(bill > 500){
-    console.log(" Burger more than 500tk: free Coke")
-}else{
-    bill += 30;
-    console.log(`total bill ${bill}`)
-}
+// let bill = 600;
+// if(bill >= 500){
+//     console.log(" Burger more than 500tk: free Coke")
+// }else{
+//     bill += 30;
+//     console.log(`total bill ${bill}`)
+// }
 
 
 
@@ -37,10 +37,26 @@ Create a JavaScript program that calculates the Body Mass Index (BMI) and assign
 */
 
 
+// let width = 60;
+// let height = 65;
 
+// let min = height * 0.0254
+// // console.log(min)
+// let BMI = width / min ** 2
+// // console.log(BMI)
 
+// // - BMI < 18.5, you are underweight.
 
+// if(BMI < 18.5){
+//     console.log("you are underweight.")
+// }else if(BMI >= 18.5 && BMI <=24.9){
+//     console.log(" you are normal.")
+// }else if(BMI >=25 && BMI <= 29.9){
+//     console.log("You are overheight")
 
+// }else{
+//     console.log(" you are obese.")
+// }
 
 /***
 
@@ -56,10 +72,25 @@ Create a simple JavaScript program that takes a student's score as input and ret
 
 ***/
 
+// let greater = 60;
 
+// if(greater >= 90 && greater <= 100){
+//     console.log("A")
+// }
+// else if(greater >= 80 && greater <= 89){
+//     console.log("B")
+// }
 
+// else if(greater >= 70 && greater <= 79){
+//     console.log("C")
+// }
 
-
+// else if(greater >= 60 && greater <= 69){
+//     console.log("D")
+// }
+// else{
+//     console.log("Fail")
+// }
 
 
 
@@ -76,7 +107,23 @@ Note:
 use nested if-else-if-else
 */
 
+// let yourScore  = 80;
+// let friendScore = 5;
 
+// if(yourScore  >= 80){
+//     if(friendScore >= 80 ){
+//         console.log("Go for a lunch. ")
+//     }
+//     else if(friendScore >= 60){
+//         console.log("good luck next time.")
+//     }else if(friendScore >= 40){
+//         console.log(" your friend's message unseen.")
+//     }   else{
+//         console.log(" block your friend")
+//     }
+// }else{
+//     console.log("go to home and sleep and act sad")
+// }
 
 
 
@@ -95,12 +142,24 @@ if num1 is bigger than num2 then result will be double of num1. if not, then the
 write a simple if-else. 
 
 also, write it using ternary operator.
+*/
 
- */
+let num1 = 30;
+let num2 = 20;
+let result;
 
+if(num1 >= num2){
+    result = num1 + num1
+}
+else{
+    result = num1 + num2
+}
 
+console.log(result)
 
-
+// ternary oparetor
+let terResult = num1 >= num2 ? num1 *2 : num1 + num2;
+console.log(terResult)
 
 
 /***
@@ -111,3 +170,21 @@ Ticket fare Calculator
     - Senior citizens (age >= 60) gets a 15% Discount
     - Otherwise Regular ticket fare 800 tk
 */
+
+
+
+let ticketPrice = 800;
+let age = 20;
+let student = false;
+let discount;
+if(age < 10){
+    discount = `${ticketPrice} --- Children tiket free`
+}else if(student){
+    discount = ticketPrice - ticketPrice * 50 / 100;
+}
+else if(age >= 60){
+    discount = ticketPrice - ticketPrice * 15 / 100;
+}else{
+    discount = `regular tiket ${ticketPrice}`
+}
+console.log(discount)
